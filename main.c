45bb9df437354e25abb1204404c5e7edd2a10bc8
Devin Zhang <dzhang10@stuy.edu>
2020-09-30 10:08:46 -0400

    updated main.c

--- main.c
+++ main.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
 
-//Euler Problem #
+//Euler Problem #1
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.*/
+
+double sum_of_multiple(){
+	double sum = 0;
+	int addend;
+
+	for(addend=3; addend < 1000; addend++){
+		if(addend%3==0 || addend%5==0){
+			sum+=addend;
+		}
+	}
+	return sum;
+}
 
 
 
+//Euler Problem #6
+/*Find the difference between the sum of the squares of the first one hundred natural
+numbers and the square of the sum. */
 
-//Euler Problem #
+double difference_of_square(){
 
+
+}
 
 
 
 int main(){
 
+	double sum = sum_of_multiple();
+	printf("Problem 1: %0.0lf\n",sum);
 
 
+	return 0;
 }
